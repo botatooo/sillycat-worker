0c6fe38f241c9f02d8c9cd03e731690d1b0bd080
Aidan Taha <botatooo@protonmail.com>
2022-11-28 16:31:37 -0500

    feat: rust

---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+use std::collections::HashMap;
+
+use worker::*;
+
+mod utils;
+
+fn log_request(req: &Request) {
+    console_log!(
+        "{} - [{}], located at: {:?}, within: {}",
+        Date::now().to_string(),
+        req.path(),
+        req.cf().coordinates().unwrap_or_default(),
+        req.cf().region().unwrap_or_else(|| "unknown region".into())
+    );
+}
+
+#[event(fetch)]
+pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
+    log_request(&req);
+
+    // Optionally, get more helpful error messages written to the console in the case of a panic.
+    utils::set_panic_hook();
+
+    // Optionally, use the Router to handle matching endpoints, use ":name" placeholders, or "*name"
+    // catch-alls to match on specific patterns. Alternatively, use `Router::with_data(D)` to
+    // provide arbitrary data that will be accessible in each route via the `ctx.data()` method.
+    let router = Router::new();
+
+    // Add as many routes as your Worker needs! Each route will get a `Request` for handling HTTP
+    // functionality and a `RouteContext` which you can use to  and get route parameters and
+    // Environment bindings like KV Stores, Durable Objects, Secrets, and Variables.
+    router
+        .get_async("/", |req, _ctx| async move {
+            // Get the value of the `color1` and `color2` query parameters from the URL.
+            let url = req.url()?;
+            let queries: HashMap<_, _> = url.query_pairs().collect();
+
+            let color1query = queries.get("color1").unwrap().to_string();
+            let color2query = queries.get("color2").unwrap().to_string();
+
+            let color1 = color1query.trim_start_matches('#');
+            let color2 = color2query.trim_start_matches('#');
+
+            return utils::generate_image(color1, color2);
+        })
+        .get("/worker-version", |_, ctx| {
+            let version = ctx.var("WORKERS_RS_VERSION")?.to_string();
+            Response::ok(version)
+        })
+        .run(req, env)
+        .await
+}
